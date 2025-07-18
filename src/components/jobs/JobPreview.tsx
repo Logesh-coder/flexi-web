f7868786c0f0eca57f926802b90ab17187be5cd3
prabhu-developer <prabhu.k.code@gmail.com>
2025-07-18 21:11:07 +0530

    call issue fix2

--- src/components/jobs/JobPreview.tsx
+++ src/components/jobs/JobPreview.tsx
@@ -11,7 +11,7 @@ export function JobPreview({ jobData }: JobPreviewProps) {
     <div className="bg-white max-md:hidden dark:bg-gray-800 p-6 rounded-lg shadow-sm border border-gray-200 dark:border-gray-700">
       <h2 className="text-xl font-semibold mb-6 text-gray-900 dark:text-white">Job Preview</h2>
 
-      <JobCard job={jobData} />
+      <JobCard type='job' job={jobData} />
 
       {/* Help text */}
       <p className="text-sm text-gray-500 dark:text-gray-400 mt-4 text-center">
